ee452381df758afad6842771466f5c2c2e42af37
Patrick Baber <patrick@p-squared.de>
2016-09-26 22:31:59 +0200

    replace build id with build number

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -ev
 
 echo "Building and running image ..."
-docker build -t patrickbaber/website:$TRAVIS_BUILD_ID .
-docker run -d -P --name mywebsite patrickbaber/website:$TRAVIS_BUILD_ID
+docker build -t patrickbaber/website:$TRAVIS_BUILD_NUMBER .
+docker run -d -P --name mywebsite patrickbaber/website:$TRAVIS_BUILD_NUMBER
 
 echo "Checking if container is running ..."
 docker ps | grep mywebsite
@@ -13,7 +13,7 @@ docker exec mywebsite nginx -t
 
 echo "Pushing image to registry ..."
 docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD
-docker push patrickbaber/website:$TRAVIS_BUILD_ID
+docker push patrickbaber/website:$TRAVIS_BUILD_NUMBER
 
 #docker service create \
 #  --replicas 3 \
