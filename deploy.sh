4850b2ef01e5fba6a8dc884aaf5db58f94d1e486
Patrick Baber <patrick@p-squared.de>
2016-09-28 19:24:46 +0200

    remove healthcheck

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,5 @@ docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD
 docker push patrickbaber/website:build-$TRAVIS_BUILD_NUMBER
 
 echo "Updating service on manager node ..."
+#docker service create -p 80:80 --replicas 2 --reserve-memory 128M --name patrickbaber_website --update-delay 10s patrickbaber/website
 #docker service update --image patrickbaber/website:build-$TRAVIS_BUILD_NUMBER patrickbaber_website
