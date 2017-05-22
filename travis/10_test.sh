f8856547252dacf878c4fb77b0ce26a196286b56
Patrick Baber <patrick@p-squared.de>
2017-05-22 17:14:52 +0200

    refactor build scripts

---
+++ travis/10_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+
+echo "Running container ..."
+docker-compose -f docker-compose.build.yml up -d
+
+echo "Checking nginx config syntax ..."
+docker-compose -f docker-compose.build.yml exec -T webserver nginx -t
+
+echo "Checking website content ..."
+docker-compose -f docker-compose.build.yml exec -T webserver curl --fail http://localhost/ | grep "Patrick Baber"
+
+echo "Stopping container ..."
+docker-compose -f docker-compose.build.yml stop
