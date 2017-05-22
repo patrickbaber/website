b693cadb33b962ed62d2ca043011f3d35db32c43
Patrick Baber <patrick@p-squared.de>
2017-05-22 17:33:26 +0200

    add environment infos in build pipeline

--- travis/00_build.sh
+++ travis/00_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ev
 
+echo "Getting environment infos ..."
+docker-compose version
+docker version
+echo "$TRAVIS_COMMIT"
+echo "$TRAVIS_BUILD_NUMBER"
+
 echo "Building image ..."
 docker-compose -f docker-compose.build.yml build \
---build-arg COMMIT=commit-test \
---build-arg BUILD=build-test \
+--build-arg COMMIT=$TRAVIS_COMMIT \
+--build-arg BUILD=$TRAVIS_BUILD_NUMBER \
 webserver
