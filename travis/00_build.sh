5dd38035e00a50cc7ba88a5fcdf2c56e6d553096
Patrick Baber <patrick@p-squared.de>
2017-05-22 17:18:51 +0200

    just testing the pipe

--- travis/00_build.sh
+++ travis/00_build.sh
@@ -3,18 +3,6 @@ set -ev
 
 echo "Building image ..."
 docker-compose -f docker-compose.build.yml build \
---build-arg COMMIT=$TRAVIS_COMMIT \
---build-arg BUILD=$TRAVIS_BUILD_NUMBER \
+--build-arg COMMIT=commit-test \
+--build-arg BUILD=build-test \
 webserver
-
-echo "Running container ..."
-docker-compose -f docker-compose.build.yml up -d
-
-echo "Checking nginx config syntax ..."
-docker-compose -f docker-compose.build.yml exec -T webserver nginx -t
-
-echo "Checking website content ..."
-docker-compose -f docker-compose.build.yml exec -T webserver curl --fail http://localhost/ | grep "Patrick Baber"
-
-echo "Stopping container ..."
-docker-compose -f docker-compose.build.yml stop
