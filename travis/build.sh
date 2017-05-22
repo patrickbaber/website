7cd1b7cef7e1f573532321d672e2c46704b04a99
Patrick Baber <patrick@p-squared.de>
2017-05-22 14:48:17 +0200

    fix build command

--- travis/build.sh
+++ travis/build.sh
@@ -2,13 +2,13 @@
 set -ev
 
 echo "Building image ..."
-docker-compose -f docker-compose.build.yml build
-
-echo "Running container ..."
-docker-compose -f docker-compose.build.yml up -d \
+docker-compose -f docker-compose.build.yml build \
 --build-arg COMMIT=$TRAVIS_COMMIT \
 --build-arg BUILD=$TRAVIS_BUILD_NUMBER \
 webserver
+
+echo "Running container ..."
+docker-compose -f docker-compose.build.yml up -d
 
 echo "Checking nginx config syntax ..."
 docker-compose -f docker-compose.build.yml exec -T webserver nginx -t
